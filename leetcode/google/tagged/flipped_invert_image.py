cbf2f75acc2858d9c644720e252a9ce5025e1cec
Alvin Chia <4261923+alvinctk@users.noreply.github.com>
2019-07-28 16:27:35 -0600

    add faster solution using Python built in tools - map

--- leetcode/google/tagged/flipped_invert_image.py
+++ leetcode/google/tagged/flipped_invert_image.py
@@ -2,6 +2,14 @@ from typing import List
 class Solution:
     def flipAndInvertImage(self, A: List[List[int]]) -> List[List[int]]:
         """
+        56ms, faster than 92.39% of Python3 submission
+        13.7 MB less than 5.11% of Python3 submission
+        """
+        return [list(map(lambda x: 0 if x else 1, row[::-1])) for row in A]
+
+
+    def flipAndInvertImage(self, A: List[List[int]]) -> List[List[int]]:
+        """
         60ms, faster than 74.21% of Python3 submission
         13.8 MB less than 5.11% of Python3 submission
         """
