febdbfd61582d6961836a48ba37a664ec733e4b3
Alvin Chia <4261923+alvinctk@users.noreply.github.com>
2019-08-09 18:32:42 -0600

    find first and last position of a target number in an ascending array

---
+++ leetcode/google/tagged/find_first_and_last_position.py
@@ -0,0 +1,49 @@
+from typing import List
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        """
+        Runtime beats 92.59% of python3 submission
+        """
+        if not nums:
+            return [-1, -1]
+        n = len(nums)
+        start, end = 0, n - 1
+        while start <= end:
+            mid = start + (end - start + 1 + 1)//2 - 1
+            left = right = -1
+            if nums[mid] == target:
+                left = right = mid
+            elif nums[start] == target:
+                left = right = start
+            elif nums[end] == target:
+                left = right = end
+
+            if 0 <= left and left < n:
+                has_left = left - 1 >= 0 and nums[left-1] == target
+                has_right = right + 1 < n and nums[right+1] == target
+                while has_left or has_right:
+                    if has_left:
+                        left -= 1
+                    if has_right:
+                        right += 1
+                    has_left = left - 1 >= 0 and nums[left-1] == target
+                    has_right = right + 1 < n and nums[right+1] == target
+
+                return [left, right]
+
+            elif nums[mid] > target:
+                # [0, mid - 1]
+                end = mid - 1
+            else:
+                # [mid + 1, n]
+                start = mid + 1
+
+        return [-1, -1]
+
+if __name__ == "__main__":
+    s = Solution()
+    numbers = [[1, 2, 3, 4, 5, 6, 7, 9, 10], [1, 2, 3, 3, 4, 5, 6], [5, 7, 7, 8, 8, 10], [5, 7, 8, 8, 10], [1, 1], [1, 1], []]
+    for x in numbers:
+        for i in range(11):
+            r = s.searchRange(x, i)
+            print("searchRange({}, {}) = {}".format(x, i, r))
