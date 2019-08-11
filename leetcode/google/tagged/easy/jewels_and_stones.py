d7c32eb756a6fed3010ea94dba9f628c71bb3b2d
Alvin Chia <4261923+alvinctk@users.noreply.github.com>
2019-08-11 16:24:29 -0600

    add improved solution for jewels and stones

--- leetcode/google/tagged/easy/jewels_and_stones.py
+++ leetcode/google/tagged/easy/jewels_and_stones.py
@@ -3,7 +3,23 @@ from typing import List
 class Solution:
     def numJewelsInStones(self, J: str, S: str) -> int:
         """
-        76% faster than python3 submission
+        98.84% faster than Python3 submission
+        Time complexity: O(n)
+            O(n) for iterating each stones
+            O(1) for checking if a stone is a jewel by using a set
+
+        Space complexity: O(n) for creating a hashable set for quick comparsion
+        """
+        jewels = set(J)
+        count = 0
+        for stone in S:
+            if stone in jewels:
+                count += 1
+        return count
+
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        """
+        9% faster than python3 submission
         """
         return sum(map(S.count, set(J)))
 
