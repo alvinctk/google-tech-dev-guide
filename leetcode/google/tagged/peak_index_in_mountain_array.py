960a1b19e1b1212952f7fc9b60e20d51c9a4e688
Alvin Chia <4261923+alvinctk@users.noreply.github.com>
2019-08-10 23:09:05 -0600

    add Peak index in a mountain array problem

---
+++ leetcode/google/tagged/peak_index_in_mountain_array.py
@@ -0,0 +1,26 @@
+class Solution:
+    def xpeakIndexInMountainArray(self, A: List[int]) -> int:
+        """
+        Time complexity = O(n)
+        """
+        n = len(A)
+        i = 0
+        while i + 1 < n and A[i] < A[i + 1]:
+            i += 1
+        return i
+
+    def peakIndexInMountainArray(self, A: List[int]) -> int:
+        """
+        Time complexity = O(log n)
+        Your runtime beats 98.91 % of python3 submissions.
+        """
+        n = len(A)
+        left, right = 1, n - 2
+        while left <= right:
+            mid = left + (right - left)//2
+            if A[mid - 1] < A[mid] > A[mid + 1]:
+                return mid
+            elif A[mid] < A[mid + 1]:
+                left = mid + 1
+            elif A[mid - 1] > A[mid]:
+                right = mid - 1
