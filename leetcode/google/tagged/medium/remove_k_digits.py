b28e93dfd440e4d61bedd41d045f77d37f526d41
Alvin Chia <4261923+alvinctk@users.noreply.github.com>
2019-08-25 15:04:02 -0700

    remove k digits to form a smaller number

---
+++ leetcode/google/tagged/medium/remove_k_digits.py
@@ -0,0 +1,47 @@
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        if k <= 0:
+            return num
+        elif not num or len(num) - k  == 0:
+            return "0"
+        debug = False
+        stack = []
+        remove = []
+        i, n = 0, len(num)
+        total = n - k
+
+        while i < n:
+            if debug: print("i={} k={} num[{}]={} stack={} remove={}".format(i, k, i, num[i], stack, remove))
+            if not stack:
+                if num[i] == "0":
+                    k -= 1
+                    remove.append("0")
+                else:
+                    stack.append(num[i])
+                i += 1
+                continue
+
+            if stack[-1] > num[i]:
+                if k > 0:
+                    k -= 1
+                    remove.append(stack.pop())
+                    while not stack and i < n and num[i] == "0":
+                        i += 1
+                    continue
+                else:
+                    stack.append(num[i])
+                    i += 1
+            elif stack[-1] < num[i]:
+                if len(stack) < total:
+                    stack.append(num[i])
+                i += 1
+
+            else:
+                stack.append(num[i])
+                i += 1
+
+        while stack and len(stack) > total:
+            if debug: print("stack={}".format(stack))
+            stack.pop()
+
+        return "".join(stack) if stack else "0"
