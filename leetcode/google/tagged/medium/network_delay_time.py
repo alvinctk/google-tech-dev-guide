b574edbc7db3db379c91d57bb67fb19decaec346
Alvin Chia <4261923+alvinctk@users.noreply.github.com>
2019-08-25 18:08:28 -0700

    add network delay time

---
+++ leetcode/google/tagged/medium/network_delay_time.py
@@ -0,0 +1,19 @@
+from heapq import heappop
+from heapq import heappush
+from collections import defaultdict
+class Solution:
+    def networkDelayTime(self, times, N, K):
+        q, seen, adj = [(0, K)], {}, collections.defaultdict(list)
+
+        for u, v, w in times:
+            adj[u].append((v, w))
+
+        while q:
+            time, node = heappop(q)
+            if node not in seen:
+                seen[node] = time
+
+                for v, w in adj[node]:
+                    if v not in seen: heappush(q, (time + w, v))
+
+        return max(seen.values()) if len(seen) == N else -1
